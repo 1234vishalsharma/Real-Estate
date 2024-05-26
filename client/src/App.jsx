0c129dacd1fd028d4b771412c8df46a22a9d76d2
Vishal Sharma <vishalsharma15122003@gmail.com>
2024-05-26 14:16:04 +0530

    project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='bg-red-500 w-96 h-96 text-white font-bold'>this is a div.</div>
+    </>
+  )
+}
+
+export default App
